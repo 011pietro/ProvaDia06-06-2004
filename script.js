40c7f9118c0ea7d281fffa5d48f7b60476fc09fb
011pietro <166553675+011pietro@users.noreply.github.com>
2024-06-06 16:28:35 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,50 @@
+let NomedaPessoa = document.getElementById("nome");
+let resposta = document.getElementById("resposta");
+
+function Enviar ()
+{
+    resposta.innerHTML = "Olá, " +
+    nome.value + " Tudo bem?"
+}
+let numero = document.getElementById ("numero")
+let calculo = document.getElementById ("calculo")
+
+function calcule()
+{
+    calculo.innerHTML = parseInt(numero.value) /100
+}
+//atividade 7
+b = 1  
+while(b <= 100 )
+    {
+        console.log(b)
+        b = b +1;   
+    }
+
+//atividade 8
+c = 0
+while(c <= 50)
+    {
+        console.log(c)
+        c = c +5;
+    }
+//atividade 9
+d = 100
+
+while(d >= 0 )
+        {
+            console.log(d)
+            d = d -10;
+        }
+//atividade 5
+let nm1 = document.getElementById("nm1")
+let nm2 = document.getElementById("nm2")
+let somar = document.getElementById("somar")
+let resultado = document.getElementById("resultado")
+
+function resultado ()
+{
+    
+
+
+}
